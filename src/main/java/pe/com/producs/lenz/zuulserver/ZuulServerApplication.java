14f7909ef47989dd502f8256e70cde7a24bbc5c1
lenz90 <lenz.90@gmail.com>
2019-12-29 16:43:48 -0500

    initial commit

---
+++ src/main/java/pe/com/producs/lenz/zuulserver/ZuulServerApplication.java
@@ -0,0 +1,13 @@
+package pe.com.producs.lenz.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+
+}
